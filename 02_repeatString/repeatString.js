423d0bc0b6a36456db432d625e90f69fc62960fd
Alex <118135714+Alex-Nemet@users.noreply.github.com>
2023-02-04 19:11:49 -0400

    Finish exercise 0 and 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+let greeting = '';
 
-};
+const repeatString = function(string, num) {
+    
+    if (num < 0 ) {
+        return 'ERROR'
+    } else {
+        for (let i = 0; i < num; i++) {
+            greeting += string;
+        }
+        return greeting;
+
+    }
+    
+    
+    };
 
 // Do not edit below this line
 module.exports = repeatString;
