ee417752a46486be9f526904aca753d8db2ae509
Alex <118135714+Alex-Nemet@users.noreply.github.com>
2023-02-17 15:17:08 -0400

    first time editing and passing test in nvim

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+let lastLetter 
+let reversedString = ""
+
+
+const reverseString = function(inputString) {
+
+  for (let i = 1; i < inputString.length +1; i++) {
+  lastLetter = inputString.charAt(inputString.length - i);
+  reversedString += lastLetter;
+}
+
+return reversedString;
 
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
--- 03_reverseString/testjava.js
+++ 03_reverseString/testjava.js
@@ -1,4 +1,19 @@
-let myString = "hello, world"
-console.log(myString);
+let lastLetter 
+let reversedString = ""
 
+
+const reverseString = function(inputString) {
+
+  for (let i = 1; i < inputString.length +1; i++) {
+  lastLetter = inputString.charAt(inputString.length - i);
+  reversedString += lastLetter;
+}
+
+console.log(reversedString)
+
+};
+
+reverseString('Hello, World!');
+// Do not edit below this line
+module.exports = reverseString;
 
